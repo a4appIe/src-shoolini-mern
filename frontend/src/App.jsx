bdf88efb27ec93d16b482a54cf0e631839f91529
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-28 16:33:25 +0530

    navbar designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Services from "./pages/Services"
+import Contact from "./pages/Contact"
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services/>} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import SRC_LOGO from "/src_logo.png";
+import { MdMenu } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, []);
+
+  const handleNavItemClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <nav className="bg-white shadow-md sticky top-0 px-14 flex items-center justify-between py-2 max-sm:px-5 max-md:px-8">
+        <div>
+          {/* { LOGO } */}
+          <Link to={"/"}>
+            <img src={SRC_LOGO} alt="" className="w-20 max-sm:w-16 md:w-18" />
+          </Link>
+        </div>
+        <div className="">
+          {/* { HAMBURGER_MENU } */}
+          <p>
+            <MdMenu
+              className="cursor-pointer text-2xl sm:hidden"
+              onClick={() => {
+                setIsOpen((prev) => !prev);
+              }}
+            />
+          </p>
+          <ul className="flex gap-8 max-sm:hidden">
+            <li className="text-[14px] max-md:text-sm lg:text-[15px]">
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li className="text-[14px] max-md:text-sm lg:text-[15px]">
+              <Link to={"/about"}>About</Link>
+            </li>
+            <li className="text-[14px] max-md:text-sm lg:text-[15px]">
+              <Link to={"/services"}>Services</Link>
+            </li>
+            <li className="text-[14px] max-md:text-sm lg:text-[15px]">
+              <Link to={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* MOBILE MENU */}
+      </nav>
+      <div
+        className={`flex flex-col bg-yellow-200 h-screen w-screen text-2xl fixed top-0 p-3 gap-8 ${
+          isOpen ? "block" : "hidden"
+        }`}
+      >
+        <p className="flex items-center justify-end">
+          <RxCross2
+            className="border cursor-pointer"
+            onClick={() => {
+              setIsOpen((prev) => !prev);
+            }}
+          />
+        </p>
+        <ul className="flex flex-col gap-3 px-10 justify-center">
+          <Link to={"/"}>
+            <li
+              className="text-center border cursor-pointer bg-white border-black hover:bg-blue-200 duration-700 shadow-[6px_6px_0px_0px_rgba(0,_0,_0)]"
+              onClick={handleNavItemClick}
+            >
+              Home
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li
+              className="text-center border bg-white border-black hover:bg-blue-200 duration-700 shadow-[6px_6px_0px_0px_rgba(0,_0,_0)]"
+              onClick={handleNavItemClick}
+            >
+              About
+            </li>
+          </Link>
+          <Link to={"/services"}>
+            <li
+              className="text-center border bg-white border-black hover:bg-blue-200 duration-700 shadow-[6px_6px_0px_0px_rgba(0,_0,_0)]"
+              onClick={handleNavItemClick}
+            >
+              Services
+            </li>
+          </Link>
+          <Link to={"/contact"}>
+            <li
+              className="text-center border bg-white border-black hover:bg-blue-200 duration-700 shadow-[6px_6px_0px_0px_rgba(0,_0,_0)]"
+              onClick={handleNavItemClick}
+            >
+              Contact
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbar;
